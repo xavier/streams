9c6f8908875cbf06f13e8c91e29a480339969fff
Xavier Defrang <xavier.defrang@gmail.com>
2013-12-31 13:24:45 +0100

    Page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def page_title(*parts)
+    (parts.flatten + ["Xavier Defrang"]).join(" | ")
+  end
+
   def glyphicon(name)
     content_tag(:span, nil, class: "glyphicon glyphicon-#{name}")
   end
